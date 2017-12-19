e2ca4acc4b46ff782988e37d0342d469a0f26002
Unknown <thefreakintim@gmail.com>
2017-12-19 01:18:09 +0100

    Update GUI

--- src/de/thm/se/calculator/DialogCreator.java
+++ src/de/thm/se/calculator/DialogCreator.java
@@ -4,11 +4,11 @@ import java.util.Optional;
 
 import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.GridPane;
 import javafx.util.Pair;
 
@@ -25,12 +25,15 @@ public class DialogCreator extends Dialog{
 	    ButtonType okbutton = new ButtonType("OK", ButtonData.OK_DONE);
 	    this.getDialogPane().getButtonTypes().addAll(okbutton, ButtonType.CANCEL);
 	    GridPane gridPane = new GridPane();
+	    gridPane.setPrefSize(500, 150);
 	    gridPane.setHgap(10);
 	    gridPane.setVgap(10);
 	    gridPane.setPadding(new Insets(20, 150, 10, 10));
 	    TextField eins = new TextField();
+	    eins.setPrefSize(240, 50);
 	    eins.setPromptText(one);
 	    TextField zwei = new TextField();
+	    zwei.setPrefSize(240, 50);
 	    zwei.setPromptText(two);
 	    gridPane.add(new Label(one), 0, 0);
 	    gridPane.add(eins, 0, 1);
